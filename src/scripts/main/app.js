ec6f041810ec0131e0017e0c2ddb90759ecb9932
Alexander Selyuchenko <alexander.selyuchenko@gmail.com>
2019-10-24 21:56:38 +0300

    Convert App in userspace to JS

--- src/scripts/main/app.js
+++ src/scripts/main/app.js
@@ -1,7 +1,7 @@
 import {App as StdApp} from "@std/app";
 
 export class App extends StdApp {
-    protected constructor(config: JsonMap) {
+    constructor(config) {
         super(config);
     }
 }
