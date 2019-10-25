016284c83a9cd65e4edaead90e47ca9f031073d4
Alexander Selyuchenko <alexander.selyuchenko@gmail.com>
2019-10-25 23:05:08 +0300

    Allow watching of nunjucks templates

--- webpack.base.js
+++ webpack.base.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ExtraWatchWebpackPlugin = require("extra-watch-webpack-plugin");
 const NunjucksWebpackPlugin = require("nunjucks-webpack-plugin");
 
 const config = {
@@ -120,7 +121,17 @@ const config = {
 
                 return acc;
             }, []),
-        })
+
+            writeToFileEmit: true,
+
+            configure: {
+
+            },
+        }),
+
+        new ExtraWatchWebpackPlugin({
+            dirs: ["src/pages"],
+        }),
     ],
 
     resolve: {
