6261bc099e9f25588e8d15b061acaeffde10f738
Alexander Selyuchenko <alexander.selyuchenko@gmail.com>
2020-05-13 11:40:57 +0300

    Pass NODE_ENV to webpack

--- webpack.base.js
+++ webpack.base.js
@@ -169,6 +169,10 @@ const config = {
         new VueLoaderPlugin(),
 
         // new BundleAnalyzerPlugin(),
+
+        new webpack.DefinePlugin({
+            "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV || "development"),
+        }),
     ],
 
     resolve: {
